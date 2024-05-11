f863a1055b823ea9ce51f65121e2d62a0c487f07
Nicholas O'Neill <159676183+nicholasoneill97@users.noreply.github.com>
2024-05-10 20:07:28 -0400

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,17 +1,25 @@
 
-
+//grabbing next and prev from index.html and setting them as variable
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
+//grabbing carousel, list, thumbnail, item and time classes from index.html and setting them as variables
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
+//appends the first item in thumbnailItemsDom to thumbnailBorderDom
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+
+//sets two time variables
+//timeRunning is 3 seconds
+//timeAutoNext is 15 seconds
 let timeRunning = 3000;
 let timeAutoNext = 15000;
+
+//function that handles user clicking next or previous arrows
 
 nextDom.onclick = function(){
     showSlider('next');    
@@ -20,10 +28,19 @@ nextDom.onclick = function(){
 prevDom.onclick = function(){
     showSlider('prev');    
 }
+
+//declares runTimeOut
 let runTimeOut;
+
+//declares runNextAuto to automatically render a next click after 15 seconds
 let runNextAuto = setTimeout(() => {
     next.click();
 }, timeAutoNext)
+
+//show slider function that handles showing new slides depending on which button was clicked
+//function appends next slide to the first item of the SliderItemsDom
+//functions appends previous slide shown to the first item of the thumbnailItemsDom
+//function also has instructions to run automatically after 15 seconds
 function showSlider(type){
     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
@@ -49,7 +66,7 @@ function showSlider(type){
     }, timeAutoNext)
 };
 
-
+//function that handles the nav bar links appearing or being hidden
 
   function myFunction() {
     var x = document.getElementById("myLinks");
